a1ff7cdd5cc9c11fb0570d4cae725e1cce34464f
Ashish <ashishSingh15092001@gmail.com>
2023-10-28 19:28:42 +0530

    sdbj

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ require('./app/routes/testimonial.routes.js')(app)
 require('./app/routes/company.routes.js')(app)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || '8080'
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
